50667bd9d709dd8d7e3b8fed98cfa1b2de549cc1
imamsutono <imamssutono@gmail.com>
2018-08-11 15:29:58 +0700

    Done form with validator

---
+++ lib/main.dart
@@ -0,0 +1,116 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: MyApp(),
+  ));
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Form Registrasi"),
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          padding: EdgeInsets.all(16.0),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                TextFormField(
+                  validator: (a) {
+                    if (a.isEmpty) {
+                      return "Please fill your email address";
+                    }
+                  },
+                  decoration: InputDecoration(
+                    labelText: "Email Address",
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20.0)
+                    )
+                  ),
+                  keyboardType: TextInputType.emailAddress,
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 16.0),
+                ),
+                TextFormField(
+                  validator: (a) {
+                    if (a.isEmpty) {
+                      return "Please fill your full name";
+                    }
+                  },
+                  decoration: InputDecoration(
+                    labelText: "Full Name",
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20.0)
+                    )
+                  ),
+                  keyboardType: TextInputType.text
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 16.0),
+                ),
+                TextFormField(
+                  validator: (a) {
+                    if (a.isEmpty) {
+                      return "Please fill your phone number";
+                    }
+                  },
+                  decoration: InputDecoration(
+                    labelText: "Phone Number",
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20.0)
+                    )
+                  ),
+                  keyboardType: TextInputType.phone,
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 16.0),
+                ),
+                TextFormField(
+                  validator: (a) {
+                    if (a.isEmpty) {
+                      return "Please fill your password";
+                    }
+                  },
+                  decoration: InputDecoration(
+                    labelText: "Password",
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20.0)
+                    )
+                  ),
+                  obscureText: true,
+                  keyboardType: TextInputType.text
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 16.0),
+                ),
+                RaisedButton(
+                  onPressed: () {
+                    if (_formKey.currentState.validate()) {
+                      
+                    }
+                  },
+                  child: Text("Submit"),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
